4d442a687a66d0b488d3a590396417f1e1fcb7ab
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-24 17:45:22 -0600

    Add files via upload

---
+++ combuter.py
@@ -0,0 +1,13 @@
+class computer:
+    def __init__(self):
+        self.__maxprice = 900
+    def sell(self):
+        print("selling price: {}".format(self.__maxprice))
+    def setmaxprice(self,price):
+        self.maxprice = price
+c = computer()
+c.sell()
+c.__maxprice = 1000
+c.sell()
+c.setmaxprice(1000)
+c.sell()
