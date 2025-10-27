4fb146ac0a024e14bfa6782032b78ae826c583f0
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-27 17:42:44 -0600

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+class animal(ABC):
+    def move(self):
+        pass
+class human(animal):
+    def move(self):
+        print("I can walk and run")
+class snake(animal):
+    def move(self):
+        print("I can crawl")
+class dog(animal):
+    def move(self):
+        print("I can bark")
+class lion(animal):
+    def move(self):
+        print("I can roar")
+r = human()
+r.move()
+k = snake()
+k.move()
+r = dog()
+r.move()
+k = lion()
+k.move()
