98d20b362ff084f816b0a704f2ad4e56b513877a
shayankhalid-cmd <shayan.khalid@renacademy.org>
2025-10-27 18:01:59 -0600

    Update homework27.py

--- homework27.py
+++ homework27.py
@@ -2,6 +2,7 @@ class word:
     def __init__(self, text):
         self.text = text
     def reverse(self):
-            return self.text
+            return self.text [::-1]
 my_word = word(int(input("Enter a word: ")))
+
 print(my_word.reverse())
